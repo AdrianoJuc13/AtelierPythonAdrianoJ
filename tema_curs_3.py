3de561a88d4aa48c5716bf30f1f6b873fb6da9e4
Adriano Jucan <98047579+AdrianoJuc13@users.noreply.github.com>
2022-04-07 14:35:09 +0300

    correcting argument

--- tema_curs_3.py
+++ tema_curs_3.py
@@ -1,11 +1,11 @@
-def your_function(x):
-    sum = 0
-    for i in x:
+def your_function(*address):
+    total_sum = 0
+    for i in address:
+        # print(type(i))
         if type(i) == int:
-            sum += i
-    return sum
+            total_sum += i
+    return total_sum
 
 
 if __name__ == '__main__':
-    x = 1, 5, -3, "abc", [12, 56, "cad"]
-    print(your_function(x))
+    print(your_function(1, 5, -3, "abc", [12, 56, "cad"]))
