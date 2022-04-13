d0b53fe0a7e1719f71cf77ec7c125bc3b4b7f82d
Adriano Jucan <98047579+AdrianoJuc13@users.noreply.github.com>
2022-04-13 18:27:33 +0300

    finished extracting data from F1website

--- teme/tema1/spider.py
+++ teme/tema1/spider.py
@@ -1,17 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://lpf.ro/liga-1'
+URL = 'https://www.formula1.com/en/racing/2022.html'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-teams = []
-stage_table = soup.find('class = clasament_general white-shadow etape_meciuri')
+stage_table = soup.find_all('div', 'col-12 col-sm-6 col-lg-4 col-xl-3', limit=24)
+
+file = open('data.txt','w')
+
+for stage in stage_table:
+    # data_tracking = stage.find('a')['data-tracking']
+    text = stage.find('div', 'event-title f1--xxs').text
+    legend = stage.find('legend', 'card-title f1-uppercase f1-color--warmRed').text
+    start_date = stage.find('span', 'start-date').text
+    end_date = stage.find('span', 'end-date').text
+    try:
+        month = stage.find('span', 'month-wrapper f1-wide--xxs').text
+    except AttributeError:
+        pass
+    try:
+        title = stage.find('div', 'event-place').text
+    except KeyError:
+        title = stage.find('div', 'event-place d-block').text
+    file.write(f'The {legend} will begin in {title} between {start_date} and {end_date} in {month}:\n\t{text}')
+file.close()
 # team_rows = stage_table.find_all('tr', attrs={'echipa_row'})
-# #
-# # for team in team_rows:
-# #     team_cell = team.find('a')
-# #     team_name = team_cell.text.strip()
-# #     teams.append(team_name)
-# print(team_rows)
+# for team in team_rows:
+#     team_cell = team.find('a')
+#     team_name = team_cell.data-tracking.strip()
+#     teams.append(team_name)
+
